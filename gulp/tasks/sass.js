160188332455c0b881d1a98bf843e015d1145cc2
Dunai A <Dunai@MacBook-Pro.local>
2019-01-06 17:30:10 +0300

    styling of work, welcome and blog pages

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -3,6 +3,7 @@
 module.exports = function() {
   $.gulp.task('sass', function() {
     return $.gulp.src('./source/style/main.scss')
+      .pipe($.gp.sassBulkImport())
       .pipe($.gp.sourcemaps.init())
       .pipe($.gp.sass()).on('error', $.gp.notify.onError({ title: 'Style' }))
       .pipe($.gp.autoprefixer({ browsers: $.config.autoprefixerConfig }))
